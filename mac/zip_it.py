589be5f440d762ecca677e2e2d11e407c9a9f87b
Yotam Alon <yotamalon@gmail.com>
2022-02-13 08:51:03 +0200

    Fixing zip_it script for mac

--- mac/zip_it.py
+++ mac/zip_it.py
@@ -1,13 +1,13 @@
-from os.path import split, join
+from os.path import split, join, dirname, abspath
 from PyInstaller import __main__ as pyinst
 # from kivy_deps import sdl2, glew
 
-src_dir = split(split(__file__)[0])[0]
+src_dir = split(dirname(abspath(__file__)))[0]
 command = [
     join(src_dir, 'main.py'), '-n', 'AudioChef', '-y', '-i',  join(src_dir, 'assets', 'chef_hat.ico'),
     '--onefile',
     '--noconsole',
-    '--add-binary', f'{src_dir}/mac/ffmpeg/ffmpeg.exe:.',
+    '--add-binary', f'{src_dir}/mac/ffmpeg/ffmpeg:.',
     '--add-data', f'{src_dir}/assets:assets',
     '--add-data', f'{src_dir}/audio_chef.kv:.'
     ]
