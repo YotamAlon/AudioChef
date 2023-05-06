5a2c73b210271fbd76633f232ae31e8f6b93f773
Yotam Alon <yotamalon@gmail.com>
2023-05-06 23:06:04 +0300

    Fixed typing in State.py

--- utils/State.py
+++ utils/State.py
@@ -1,14 +1,14 @@
-from typing import Any
+import typing
 
 
 class State:
     """Basic implementation of a state object"""
-    _state = {}
+    _state: typing.ClassVar[typing.Dict[str, typing.Any]] = {}
 
-    def set_prop(self, prop: str, value: Any) -> None:
+    def set_prop(self, prop: str, value: typing.Any) -> None:
         self._state[prop] = value
 
-    def get_prop(self, prop: str) -> Any:
+    def get_prop(self, prop: str) -> typing.Any:
         return self._state.get(prop, None)
 
 
