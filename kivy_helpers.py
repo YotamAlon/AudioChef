8e59d21436227d2d55644c42fd6cdc9e90b85c50
Yotam Alon <yotamalon@gmail.com>
2024-01-06 14:47:05 +0200

    Fixed name change type toggle bug

--- kivy_helpers.py
+++ kivy_helpers.py
@@ -2,7 +2,7 @@ from kivy.uix.widget import Widget
 
 
 def toggle_widget(wid: Widget, hide: bool) -> None:
-    if hasattr(wid, 'saved_attrs'):
+    if hasattr(wid, "saved_attrs"):
         if not hide:
             wid.height, wid.size_hint_y, wid.opacity, wid.disabled = wid.saved_attrs
             del wid.saved_attrs
